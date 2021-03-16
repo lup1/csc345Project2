ca346d0ad4bfb63da47098cb2351fc39a46bf433
lup1 <54862878+lup1@users.noreply.github.com>
2021-03-16 18:44:00 -0400

    Add files via upload
    Scans values from input.txt and outputs onto terminal

--- main.c
+++ main.c
@@ -4,15 +4,29 @@
 #include <unistd.h>
 #include "string.h"
 
-extern FILE *input_file;
+//extern FILE *input_file;
+const int MAX = 82;
 
 int main(int argc, char *argv[]) {
+    FILE *input_file;
+
+    int val;      // Variable to contain value currently scanned
+    int entry[MAX];
+
+
+    int scanTest = 0;
 
     input_file = fopen(argv[argc-1],"r");
     if(!input_file) {
         printf("Couldnt open file\n");
         return 1;
     }
-    printf("Opens file?");
 
+    while(fscanf(input_file, "%d", &val) == 1) {
+        printf("%d\n",val);
+        
+    } 
+    //printf("Opens file?");
+
+return 0;
 }
