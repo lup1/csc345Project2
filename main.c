e8ff3b85dfa0fdd27da96ec2dca24f23d5131f63
lup1 <54862878+lup1@users.noreply.github.com>
2021-03-17 23:57:38 -0400

    Add files via upload

--- main.c
+++ main.c
@@ -11,14 +11,16 @@ int main(int argc, char *argv[]) {
     FILE *input_file;
 
     int val;      /* Variable to contain value currently scanned */
-    
+    int i;  /* Iterating for printing out the sequence */
 
-    int entries[81];   /* stores inputs to be arranged into rows, columns, 
+
+    int entries[81] = {0};   /* stores inputs to be arranged into rows, columns, 
                             and 3x3 sections */
     
 
     int iterate = 0;
 
+    printf("Last term before scanning: %d\n",entries[iterate]);
     input_file = fopen(argv[argc-1],"r");
     if(!input_file) {
         printf("Couldnt open file\n");
@@ -28,10 +30,32 @@ int main(int argc, char *argv[]) {
     while(fscanf(input_file, "%d", &val) == 1) {
         
         entries[iterate] = val; /* array will store all values from the input file, then the program will split the inputs up into either rows, columns, or 3x3 sections */
-        //printf("%d ",val);
-        //printf("%d\n",entries[iterate]);
+        
+        printf("%d ",entries[iterate]);
+
+        if (iterate % 9 ==8){
+            printf("\n");
+        }
+        
+        iterate++;
+        //iterate = iterate+1;
+        //count++;
+        /*for (i = 0; i < 9; i++) {
+            printf("%d ",val);
+        }*/        
+    /*printf("Term %d: %d\n", iterate, entries[iterate]);*/
         
     } 
+    printf("iterate: %d\n", iterate);
+    printf("Last term: %d\n",entries[iterate]);
+    /*
+    for(int i = 0; i < iterate; i++) {
+        printf("%d ",entries[iterate]);
+
+        if (iterate % 9 ==8){
+            printf("\n");
+        }
+    } */
     //printf("Opens file?");
 
 return 0;
